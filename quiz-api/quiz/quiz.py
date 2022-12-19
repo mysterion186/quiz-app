4c6cb01534b816a2bd2f82a17bd7a9acdb9ad169
mysterion186 <kowsikan@hotmail.fr>
2022-12-19 12:01:53 +0100

    feature : Only admin can rebuild db

--- quiz-api/quiz/quiz.py
+++ quiz-api/quiz/quiz.py
@@ -5,14 +5,22 @@ from tools import jwt_utils as jwt
 from tools import create_db
 quiz = Blueprint("quiz", __name__)
 
-@quiz.route('/rebuild-db')
+@quiz.route('/rebuild-db', methods = ['POST'])
 def init_db():
+    raw_token = request.headers.get('Authorization')
+    if raw_token == None : 
+        return 'Missing authorization headers', 401
+    token = raw_token.replace("Bearer ","")
     try : 
-        create_db.create_question(path = create_db.PATH)
-        return "ok", 200
-    except Exception as e :
+        try : 
+            create_db.create_question(path = create_db.PATH)
+            return "ok", 200
+        except Exception as e :
+            print(e)
+            return "Error", 401
+    except jwt.JwtError as e:
         print(e)
-        return "Error", 401
+        return 'Unauthorized', 401
 
 @quiz.route('/questions',methods=['POST'])
 def add_questions() : 
